c732560f35a2dcadcf42d5406d552ca905ced4a9
Greg Meltzer <vadercows@gmail.com>
2019-09-05 20:06:46 -0400

    start work on message protocol

---
+++ moppy/protocol.py
@@ -0,0 +1,85 @@
+import typing as t
+
+BYTE_LENGTH = 8
+
+MESSAGE_START = 0x4d
+
+
+class BaseMoppyCommand(object):
+    command = -1
+
+    def to_list(self):
+        payload = getattr(self, '__slots__', [])
+        payload = [getattr(self, slot) for slot in payload]
+        return [self.command] + payload
+
+
+class SystemPingCommand(BaseMoppyCommand):
+    command = 0x80
+
+
+class SystemPongCommand(BaseMoppyCommand):
+    command = 0x81
+
+    __slots__ = ['device_address', 'min_sub_address', 'max_sub_address']
+
+    def __init__(self, device_address: int, min_sub_address: int, max_sub_address: int) -> None:
+        self.device_address = device_address
+        self.min_sub_address = min_sub_address
+        self.max_sub_address = max_sub_address
+
+
+class SystemResetCommand(BaseMoppyCommand):
+    command = 0xff
+
+
+class SystemSequenceStartCommand(BaseMoppyCommand):
+    command = 0xfa
+
+
+class SystemSequenceStopCommand(BaseMoppyCommand):
+    command = 0xfc
+
+
+class DeviceResetCommand(BaseMoppyCommand):
+    command = 0x00
+
+
+class DeviceStopNote(BaseMoppyCommand):
+    command = 0x08
+
+    __slots__ = ['note_number']
+
+    def __init__(self, note_number: int) -> None:
+        self.note_number = note_number
+
+
+class DevicePlayNoteCommand(BaseMoppyCommand):
+    command = 0x09
+
+    __slots__ = ['note_number', 'velocity']
+
+    def __init__(self, note_number: int, velocity: int) -> None:
+        self.note_number = note_number
+        self.velocity = velocity
+
+
+class DeviceBendPitch(BaseMoppyCommand):
+    command = 0x0e
+
+    __slots__ = ['msb', 'lsb']
+
+    def __init__(self, msb: int, lsb: int) -> None:
+        self.msb = msb
+        self.lsb = lsb
+
+
+class MoppyMessage(t.NamedTuple):
+    device_address: int
+    sub_address: int
+    command: BaseMoppyCommand
+
+    @property
+    def length(self) -> int:
+        return len(self.command.to_list())
+
